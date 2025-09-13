e40293b97edd02d9a14e4cf78a4802b59de1ab27
SOHEL4W <sohelranasarkar643@gmail.com>
2025-09-13 19:00:30 +0530

    Updated Navbar CSS and Navbar component

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
+import navlogo from '../../assets/logo.png'
 // This is the line that has been fixed
 import navProfile from '../../assets/nav-Profile.png'
 
 export const Navbar = () => {
   return (
     <div className='navbar'>
-      <img src={navlogo} alt="" className="nav-logo" />
+      <div className="nav-logo" >
+        <img src={navlogo} alt="logo" />
+        <p>GODZILA</p>
+      </div>
       <img src={navProfile} alt="" className="nav-profile" />
     </div>
   )
 }
 
 export default Navbar
+/*<div className={Classes.nav_logo}>
+        <img src={logo} alt="logo" />
+        <p>GODZILA</p>
+      </div> <img src={navlogo} alt="" className="nav-logo" />*/
